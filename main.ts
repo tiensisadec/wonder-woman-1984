c55e02a4daf83aea2af21fdb442c37892eb9e923
tiensisadec <43952125+tiensisadec@users.noreply.github.com>
2020-09-20 20:37:31 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,32 +3,33 @@ namespace SpriteKind {
 }
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Coin, function (sprite, otherSprite) {
     otherSprite.destroy()
+    info.changeScoreBy(1)
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.castle.saplingOak, function (sprite, location) {
     game.over(true)
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    game.over(false)
+})
+let ngoc: Sprite = null
 let key: Sprite = null
 let mai_rap = sprites.create(img`
-    . . . . . . f f f f . . . . . . 
-    . . . . f f f 2 2 f f f . . . . 
-    . . . f f f 2 2 2 2 f f f . . . 
-    . . f f f e e e e e e f f f . . 
-    . . f f e 2 2 2 2 2 2 e e f . . 
-    . . f e 2 f f f f f f 2 e f . . 
-    . . f f f f e e e e f f f f . . 
-    . f f e f b f 4 4 f b f e f f . 
-    . f e e 4 1 f d d f 1 4 e e f . 
-    . . f f f f d d d d d e e f . . 
-    . f d d d d f 4 4 4 e e f . . . 
-    . f b b b b f 2 2 2 2 f 4 e . . 
-    . f b b b b f 2 2 2 2 f d 4 . . 
-    . . f c c f 4 5 5 4 4 f 4 4 . . 
-    . . . f f f f f f f f . . . . . 
-    . . . . . f f . . f f . . . . . 
+    . . . . f f f f . . . . 
+    . . f f 1 1 1 1 f f . . 
+    . f b 1 1 1 1 1 1 b f . 
+    . f 1 1 1 1 1 1 1 1 f . 
+    f d 1 1 1 1 1 1 1 1 d f 
+    f d 1 1 1 1 1 1 1 1 d f 
+    f d d d 1 1 1 1 d d d f 
+    f b d b f d d f b d b f 
+    f c d c f 1 1 f c d c f 
+    . f b 1 1 1 1 1 1 f f f 
+    f f f c d b 1 b c 1 1 1 
+    1 1 1 c b f b f 1 b 1 b 
     `, SpriteKind.Player)
-controller.moveSprite(mai_rap, 150, 150)
+controller.moveSprite(mai_rap, 100, 100)
 scene.setBackgroundColor(7)
-tiles.setTilemap(tiles.createTilemap(hex`1000100001020101010101010101010101010101010001040000000000000000010101010100010001010100010101040000010101000100000101040101010101000101010001010000010001010101010001010104010101040000010101010000010101000101010101000101010004010103010001010101010001010000010100040104010101010000010000010101000101000101010100010100010101010001010001010101040000000101010000010100000004000001010004000000010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101`, img`
+tiles.setTilemap(tiles.createTilemap(hex`1000100001020101010101010101010101010101010001040000000000000000010101010100010001010100010101040000010101000105000101040101010101000101010001010000010001010101010501010104010101040000010101010000010101000101010101000101010004010103010001010101010001010000010100040104010101010000010000010101000101000101010100010100010101010001010001010101040000000101010000010100000004000001010004000000010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101`, img`
     2 . 2 2 2 2 2 2 2 2 2 2 . . . . 
     2 . 2 . . . . . . . . . 2 2 . . 
     2 . 2 . 2 2 2 . 2 2 2 . . . 2 . 
@@ -45,7 +46,7 @@ tiles.setTilemap(tiles.createTilemap(hex`100010000102010101010101010101010101010
     2 2 2 . . . . . . . . . . . . . 
     . . 2 2 2 2 2 2 2 . . . . . . . 
     . . . . . . . . . . . . . . . . 
-    `, [myTiles.transparency16,sprites.builtin.brick,sprites.castle.rock0,sprites.castle.saplingOak,myTiles.tile1], TileScale.Sixteen))
+    `, [myTiles.transparency16,sprites.builtin.brick,sprites.castle.rock0,sprites.castle.saplingOak,myTiles.tile1,sprites.dungeon.collectibleBlueCrystal], TileScale.Sixteen))
 tiles.placeOnRandomTile(mai_rap, sprites.castle.rock0)
 scene.cameraFollowSprite(mai_rap)
 for (let value of tiles.getTilesByType(myTiles.tile1)) {
@@ -53,21 +54,43 @@ for (let value of tiles.getTilesByType(myTiles.tile1)) {
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
-        . . . . . . 5 5 5 5 . . . . . . 
-        . . . . . 5 5 . . 5 5 . . . . . 
-        . . . . . 5 . . . . 5 . . . . . 
-        . . . . . 5 . . . . 5 . . . . . 
-        . . . . . 5 5 . . . 5 . . . . . 
-        . . . . . . 5 5 . 5 5 . . . . . 
-        . . . . . . . 5 5 5 . . . . . . 
-        . . . . . . . . 5 . . . . . . . 
-        . . . . . . . . 5 . . . . . . . 
-        . . . . . 5 5 5 5 . . . . . . . 
-        . . . . . . . . 5 . . . . . . . 
         . . . . . . . . . . . . . . . . 
+        . . . . 5 5 5 5 5 5 5 5 . . . . 
+        . . . . 5 . . . . . . 5 . . . . 
+        . . . . 5 . . . . . . 5 . . . . 
+        . . . . 5 . . . . . . 5 . . . . 
+        . . . . 5 5 5 5 5 5 5 5 . . . . 
+        . . . . . . . . 5 . . . . . . . 
+        . . . . . . . . 5 . . . . . . . 
+        . . . . . . . . 5 . . . . . . . 
+        . . . . . . 5 5 5 . . . . . . . 
+        . . . . . . . . 5 . . . . . . . 
+        . . . . . . . . 5 . . . . . . . 
         . . . . . . . . . . . . . . . . 
         `, SpriteKind.Coin)
     tiles.placeOnTile(key, value)
     tiles.setTileAt(value, myTiles.transparency16)
 }
 info.setScore(0)
+for (let value of tiles.getTilesByType(sprites.dungeon.collectibleBlueCrystal)) {
+    ngoc = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . 9 9 9 9 9 9 9 . . . . . 
+        . . . 9 9 8 8 8 8 8 9 9 . . . . 
+        . . 9 9 9 9 9 9 9 9 9 9 9 . . . 
+        . . 9 9 9 9 9 9 9 9 8 9 9 . . . 
+        . . 9 9 9 9 9 9 9 9 8 9 9 . . . 
+        . . 9 9 9 9 9 9 9 9 8 9 9 . . . 
+        . . 9 9 9 9 9 9 9 9 8 9 9 . . . 
+        . . 9 9 9 9 9 9 9 9 9 9 9 . . . 
+        . . 9 9 8 8 8 8 8 9 9 9 9 . . . 
+        . . . 9 9 9 9 9 9 9 9 9 . . . . 
+        . . . . 9 9 9 9 9 9 9 . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    tiles.placeOnTile(ngoc, value)
+    tiles.setTileAt(value, myTiles.transparency16)
+}
